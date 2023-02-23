4a6017b144bde0b79e84eee49f6a8eddb2f14f69
arkiver <arkiver@protonmail.com>
2023-02-23 01:33:57 +0100

    Version 20230223.02. Fix PROJECT_ID -> TRACKER_ID variable name.

--- pipeline.py
+++ pipeline.py
@@ -59,7 +59,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230223.01'
+VERSION = '20230223.02'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 TRACKER_ID = 'issuu'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -269,7 +269,7 @@ project = Project(
 pipeline = Pipeline(
     CheckIP(),
     GetItemFromTracker('http://{}/{}/multi={}/'
-        .format(TRACKER_HOST, PROJECT_ID, MULTI_ITEM_SIZE),
+        .format(TRACKER_HOST, TRACKER_ID, MULTI_ITEM_SIZE),
         downloader, VERSION),
     PrepareDirectories(warc_prefix=TRACKER_ID),
     WgetDownload(
